08840a583f00eadbbc6f70cc9d84b1f2043dda15
Miraziz <wiut00016603@gmail.com>
2023-11-04 12:07:21 +0500

    Second

--- main.py
+++ main.py
@@ -8,3 +8,13 @@ t3=tuple('salam')
 print(t3[0])
 print(t[1:3])
 print((16640, 18, 'Tsoy') > (16045, 18, 'Nikita'))
+
+def has_match(t1,t2):
+    for x,y in zip(t1,t2):
+        if x==y:
+            return True
+        return False
+
+t1 = [('Name', 'Armani'), ('Age' , 19)]
+t2 = [('Name', 'Tsoy'), ('Age' , 20)]
+has_match(t1,t2)
